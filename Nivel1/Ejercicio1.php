4163d0c9669558f519253d961d7267aeaeedea34
francobrida <francobrida@gmail.com>
2025-12-04 10:47:13 +0100

    First Version

---
+++ Nivel1/Ejercicio1.php
@@ -0,0 +1,44 @@
+<?php
+
+class Employee {
+
+    private $name;
+    private $salary;
+
+    public function __construct ($name, $salary){
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+    // Getters 
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getSalary() {
+        return $this->salary;
+    }
+
+    // Setters
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setSalary($salary) {
+        $this->salary = $salary;
+    }
+
+
+    public function checkTaxes(){
+        $message = $this->name . "<br>";
+        
+        $message .= ($this->salary > 6000)? "Paga Impuestos" : "No paga impuestos";
+        return $message;
+    }
+
+}
+
+
+?>
